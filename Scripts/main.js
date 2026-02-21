ad0e2ec51234ef616bfe1441f9730d71cd0464dd
LingkonDash <cocrupon@gamil.com>
2026-02-21 23:08:39 +0600

    Added main section

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+function showOnly(id) {
+  // buttons
+  document.getElementById('btn1').classList.remove('btn-primary')
+  document.getElementById('btn3').classList.remove('btn-primary')
+  document.getElementById('btn2').classList.remove('btn-primary')
+
+  document.getElementById(id).classList.add('btn-primary')
+}
